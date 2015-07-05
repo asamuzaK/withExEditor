dd564c80f5b1e4d853617fc34fb1eada14ff0d98
asamuzaK (Kazz) <kazz@asamuzak.jp>
2015-07-06 04:52:27 +0900

    v0.17.3

--- data/js/contentScript.js
+++ data/js/contentScript.js
@@ -3,22 +3,26 @@
 */
 (function() {
 	"use strict";
-	/* get target element and sync text value */
 	const DATA_ID = "data-with_ex_editor_id";
+
 	var element = document.activeElement,
 		target = self.options["target"] || "",
 		value = self.options["value"] || "";
-	function setContentEditableText(target, array) {
+
+	/* set content editable element text */
+	function setContentEditableText(node, array) {
 		for(var nodes = document.createDocumentFragment(), i = 0, l = array.length; i < l; i++) {
 			nodes.appendChild(document.createTextNode(array[i]));
 			i < l - 1 && nodes.appendChild(document.createElement("br"));
 		}
-		if(target.hasChildNodes()) {
-			for(i = target.childNodes.length - 1, l = 0; i >= 0; i--) {
-				target.removeChild(target.childNodes[i]);
+		if(node.hasChildNodes()) {
+			for(i = node.childNodes.length - 1, l = 0; i >= 0; i--) {
+				node.removeChild(node.childNodes[i]);
 			}
 		}
-		target.appendChild(nodes);
+		node.appendChild(nodes);
 	}
+
+	/* get target element and and sync text value */
 	element.hasAttribute(DATA_ID) && element.getAttribute(DATA_ID) === target && (/^(?:input|textarea)$/i.test(element.nodeName) ? element.value = value : element.contentEditable && setContentEditableText(element, value.split("\n")));
 })();
