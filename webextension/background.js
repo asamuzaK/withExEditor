88c13efe154bb1390edf57abbbd0372c60a1f431
asamuzaK (Kazz) <kazz@asamuzak.jp>
2016-11-15 22:18:02 +0900

    Fix icon path

--- webextension/background.js
+++ webextension/background.js
@@ -5,7 +5,7 @@
 {
   /* constants */
   const LABEL = "withExEditor";
-  const ICON_PATH = "./img/icon.svg#gray";
+  const ICON_PATH = "./img/icon.svg";
   const ICON_COLOR = "buttonIcon";
   const ICON_GRAY = "buttonIconGray";
   const ICON_WHITE = "buttonIconWhite";
@@ -42,7 +42,7 @@
   const port = runtime.connect({name: PORT_BACKGROUND});
 
   /* variables */
-  let enablePB = false, isEnabled = false, iconPath = ICON_PATH;
+  let enablePB = false, isEnabled = false, iconPath = `${ICON_PATH}#gray`;
 
   /**
    * log error
