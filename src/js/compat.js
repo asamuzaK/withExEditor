f576efbd45365c31f69636e7e492aa618e809b9f
asamuzaK (Kazz) <kazz@asamuzak.jp>
2018-07-20 14:22:07 +0900

    Revert "Refactor command (#86)"
    This reverts commit 00f3c3978d357c1e09a6b0bbb1422df81aaebded.

--- src/js/compat.js
+++ src/js/compat.js
@@ -11,16 +11,7 @@
   const EXT_WEBEXT = "jid1-WiAigu4HIo0Tag@jetpack";
   const ICON_AUTO = "buttonIconAuto";
   const KEY_ACCESS = "accessKey";
-  const MOD_KEYS_MAX = 2;
   const OPTIONS_OPEN = "openOptionsPage";
-  const TYPE_FROM = 8;
-  const TYPE_TO = -1;
-  const VERSION_PART =
-    "(?:0|[1-9]\\d{0,3}|[1-5]\\d{4}|6(?:[0-4]\\d{3}|5(?:[0-4]\\d{2}|5(?:[0-2]\\d|3[0-5]))))";
-  const VERSION_TOOLKIT =
-    `(${VERSION_PART}(?:\\.${VERSION_PART}){1,3})([A-z]+(?:-?[A-z\\d]+)?)?`;
-  const VERSION_TOOLKIT_REGEXP = new RegExp(`^(?:${VERSION_TOOLKIT})$`);
-  const WEBEXT_COMPAT_CMD_MIN = 63;
 
   /**
    * throw error
@@ -37,73 +28,6 @@
    * @returns {boolean} - result
    */
   const isString = o => typeof o === "string" || o instanceof String;
-
-  /**
-   * get type
-   * @param {*} o - object to check
-   * @returns {string} - type of object
-   */
-  const getType = o =>
-    Object.prototype.toString.call(o).slice(TYPE_FROM, TYPE_TO);
-
-  /**
-   * parse stringified integer
-   * @param {string} i - stringified integer
-   * @param {boolean} [zero] - accept leading zero
-   * @returns {number} - integer
-   */
-  const parseStringifiedInt = (i, zero = false) => {
-    if (!isString(i)) {
-      throw new TypeError(`Expexted String but got ${getType(i)}`);
-    }
-    if (!zero && !/^-?(?:0|[1-9]\d*)$/.test(i)) {
-      throw new Error(`${i} is not a stringified integer.`);
-    }
-    return parseInt(i);
-  };
-
-  /**
-   * is valid Toolkit version string
-   * @param {string} version - version string
-   * @returns {boolean} - result
-   */
-  const isValidToolkitVersion = version => {
-    if (!isString(version)) {
-      throw new TypeError(`Expected String but got ${getType(version)}`);
-    }
-    return VERSION_TOOLKIT_REGEXP.test(version);
-  };
-
-  /**
-   * parse version string
-   * @param {string} version - version string
-   * @returns {Object}
-   *   - result which contains properties below
-   *     version {string} - given version string
-   *     major {number} - major version
-   *     minor {number|undefined} - minor version
-   *     patch {number|undefined} - patch version
-   *     build {number|undefined} - build version
-   *     pre {Array<string|number>|undefined} - pre release version in array
-   */
-  const parseVersion = version => {
-    if (!isString(version)) {
-      throw new TypeError(`Expected String but got ${getType(version)}`);
-    }
-    if (!isValidToolkitVersion(version)) {
-      throw new Error(`${version} does not match toolkit format.`);
-    }
-    const [, vRelease, vPre] = version.match(VERSION_TOOLKIT_REGEXP);
-    const [major, minor, patch, build] =
-      vRelease.split(".").map(parseStringifiedInt);
-    let pre;
-    if (vPre) {
-      pre = [vPre];
-    }
-    return {
-      version, major, minor, patch, build, pre,
-    };
-  };
 
   /**
    * dispatch change event
@@ -150,7 +74,7 @@
       if (isString(id) && isString(value)) {
         const shortcut =
           value.trim().replace(/\+([a-z])$/, (m, c) => `+${c.toUpperCase()}`);
-        if (/^(?:(?:(?:(?:Alt|Command|(?:Mac)?Ctrl)(?:\+Shift)?|Alt\+(?:Command|(?:Mac)?Ctrl)|Command\+(?:Alt|MacCtrl)|Ctrl\+(?:Alt|MacCtrl)|MacCtrl\+(?:Alt|Command|Ctrl))\+(?:[\dA-Z]|F(?:[1-9]|1[0-2])|(?:Page)?(?:Down|Up)|Left|Right|Comma|Period|Home|End|Delete|Insert|Space))|F(?:[1-9]|1[0-2]))?$/.test(shortcut)) {
+        if (/^(?:(?:(?:Alt|Command|(?:Mac)?Ctrl)\+(?:Shift\+)?(?:[\dA-Z]|F(?:[1-9]|1[0-2])|(?:Page)?(?:Down|Up)|Left|Right|Comma|Period|Home|End|Delete|Insert|Space))|F(?:[1-9]|1[0-2]))$/.test(shortcut)) {
           await commands.update({
             shortcut,
             name: id,
@@ -172,66 +96,41 @@
   const detectKeyCombo = async evt => {
     const {altKey, ctrlKey, key, metaKey, shiftKey, target} = evt;
     const {disabled} = target;
-    if (!disabled && typeof runtime.getBrowserInfo === "function") {
-      const {version} = await runtime.getBrowserInfo();
-      const {major: majorVersion} = await parseVersion(version);
+    if (!disabled &&
+        /^(?:[\dA-z,. ]|Arrow(?:Down|Left|Right|Up)|F(?:[1-9]|1[0-2])|Page(?:Down|Up)|Home|End|Insert|Delete)$/.test(key)) {
       const {os} = await runtime.getPlatformInfo();
       const isMac = os === "mac";
-      const modKeys = {
-        altKey: {
-          value: "Alt",
-        },
-        ctrlKey: {
-          value: "Ctrl",
-          macValue: "MacCtrl",
-        },
-        metaKey: {
-          macValue: "Command",
-        },
-        shiftKey: {
-          value: "Shift",
-        },
-      };
-      const cmd = [];
-      // Media keys
-      if (/^Media(?:(?:Next|Prev)Track|PlayPause|Stop)$/.test(key) &&
-          !altKey && !ctrlKey && !metaKey && !shiftKey) {
-        target.value = key;
-        dispatchInputEvt(target);
-      // Function keys
-      } else if (/^F(?:[1-9]|1[0-2])$/.test(key)) {
-        ctrlKey &&
+      if (altKey && !ctrlKey || !altKey && ctrlKey || isMac && metaKey) {
+        const modKeys = {
+          altKey: {
+            value: "Alt",
+          },
+          ctrlKey: {
+            value: "Ctrl",
+            macValue: "MacCtrl",
+          },
+          metaKey: {
+            macValue: "Command",
+          },
+          shiftKey: {
+            value: "Shift",
+          },
+        };
+        const cmd = [];
+        if (altKey) {
+          cmd.push(modKeys.altKey.value);
+        } else if (ctrlKey) {
           cmd.push(isMac && modKeys.ctrlKey.macValue || modKeys.ctrlKey.value);
-        altKey && cmd.push(modKeys.altKey.value);
-        isMac && metaKey &&
+        } else if (isMac && metaKey) {
           cmd.push(modKeys.metaKey.macValue);
-        if (cmd.length < MOD_KEYS_MAX ||
-            majorVersion >= WEBEXT_COMPAT_CMD_MIN) {
-          shiftKey && cmd.push(modKeys.shiftKey.value);
-          if (cmd.length <= MOD_KEYS_MAX) {
-            cmd.push(key);
-            target.value = cmd.join("+");
-            dispatchInputEvt(target);
-          }
         }
-      // Other keys
-      } else if (/^(?:[\dA-z,. ]|Arrow(?:Down|Left|Right|Up)|F(?:[1-9]|1[0-2])|Page(?:Down|Up)|Home|End|Insert|Delete)$/.test(key)) {
-        ctrlKey &&
-          cmd.push(isMac && modKeys.ctrlKey.macValue || modKeys.ctrlKey.value);
-        altKey && cmd.push(modKeys.altKey.value);
-        isMac && metaKey && cmd.push(modKeys.metaKey.macValue);
-        if (cmd.length === 1 ||
-            majorVersion >= WEBEXT_COMPAT_CMD_MIN && cmd.length) {
-          shiftKey && cmd.push(modKeys.shiftKey.value);
-          if (cmd.length <= MOD_KEYS_MAX) {
-            cmd.push(/^[a-z]$/.test(key) && key.toUpperCase() ||
-                     /^,$/.test(key) && "Comma" ||
-                     /^\.$/.test(key) && "Period" ||
-                     /^ $/.test(key) && "Space" || key.replace(/^Arrow/, ""));
-            target.value = cmd.join("+");
-            dispatchInputEvt(target);
-          }
-        }
+        shiftKey && cmd.push(modKeys.shiftKey.value);
+        // FIXME:
+        cmd.push(/^[a-z]$/.test(key) && key.toUpperCase() ||
+                 /^,$/.test(key) && "Comma" || /^\.$/.test(key) && "Period" ||
+                 /^ $/.test(key) && "Space" || key.replace(/^Arrow/, ""));
+        target.value = cmd.join("+");
+        dispatchInputEvt(target);
       }
     }
   };
