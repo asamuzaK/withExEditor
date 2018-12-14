8196bddc2fdff83f61691dfb96a1102b36ef7d9a
asamuzaK (Kazz) <kazz@asamuzak.jp>
2018-12-14 15:23:08 +0900

    Add tests

---
+++ test/file-ext.test.js
@@ -0,0 +1,65 @@
+/**
+ * file-ext.test.js
+ */
+
+import {assert} from "chai";
+import {describe, it} from "mocha";
+import fileExt from "../src/mjs/file-ext.js";
+
+describe("file-ext", () => {
+  it("should get key", () => {
+    const itemKeys = ["application", "image", "text"];
+    const items = Object.entries(fileExt);
+    for (const [key, value] of items) {
+      assert.isTrue(itemKeys.includes(key));
+    }
+  });
+
+  describe("application", () => {
+    it("should get string", () => {
+      const subItemKeys = ["json", "xml"];
+      const items = Object.entries(fileExt.application);
+      for (const [key, value] of items) {
+        assert.isString(key);
+        if (subItemKeys.includes(key)) {
+          const subItems = Object.entries(value);
+          for (const [subKey, subValue] of subItems) {
+            assert.isString(subKey);
+            assert.isString(subValue);
+          }
+        } else {
+          assert.isString(value);
+        }
+      }
+    });
+  });
+
+  describe("image", () => {
+    it("should get string", () => {
+      const subItemKeys = ["xml"];
+      const items = Object.entries(fileExt.image);
+      for (const [key, value] of items) {
+        assert.isString(key);
+        if (subItemKeys.includes(key)) {
+          const subItems = Object.entries(value);
+          for (const [subKey, subValue] of subItems) {
+            assert.isString(subKey);
+            assert.isString(subValue);
+          }
+        } else {
+          assert.isString(value);
+        }
+      }
+    });
+  });
+
+  describe("text", () => {
+    it("should get string", () => {
+      const items = Object.entries(fileExt.text);
+      for (const [key, value] of items) {
+        assert.isString(key);
+        assert.isString(value);
+      }
+    });
+  });
+});
---
+++ test/live-edit.test.js
@@ -0,0 +1,20 @@
+/**
+ * live-edit.test.js
+ */
+
+import {assert} from "chai";
+import {describe, it} from "mocha";
+import liveEdit from "../src/mjs/live-edit.js";
+
+describe("live-edit", () => {
+  it("should get key and value", () => {
+    const itemKeys = ["aceEditor", "codeMirror"];
+    const items = Object.entries(liveEdit);
+    for (const [key, value] of items) {
+      assert.isTrue(itemKeys.includes(key));
+      assert.isTrue(value.hasOwnProperty("className"));
+      assert.isTrue(value.hasOwnProperty("getContent"));
+      assert.isTrue(value.hasOwnProperty("setContent"));
+    }
+  });
+});
