0122257f851ccc2037159fd16d382b95efc443a7
asamuzaK (Kazz) <kazz@asamuzak.jp>
2019-03-03 01:29:35 +0900

    Refactor postMsg()

--- src/js/content.js
+++ src/js/content.js
@@ -840,7 +840,7 @@
    * @returns {void}
    */
   const postMsg = async msg => {
-    if (port && msg) {
+    if (port && msg && vars[ID_TAB] && vars[ID_WIN]) {
       port.postMessage(msg);
     }
   };
