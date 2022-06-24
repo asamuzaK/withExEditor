97dc5c6b0648dba19600ad6d278fbe0a32712ba3
asamuzaK (Kazz) <kazz@asamuzak.jp>
2022-06-24 15:57:58 +0900

    Update content-loader.js

--- src/mjs/content-loader.js
+++ src/mjs/content-loader.js
@@ -4,15 +4,14 @@
 
 /* shared */
 import {
-  handleBeforeContextMenu, handleConnectedPort, handleKeyDown, handleReadyState,
-  runtimeOnMsg, startup
+  handleBeforeContextMenu, handleKeyDown, handleReadyState, runtimeOnMsg,
+  startup
 } from './content-main.js';
 
 /* api */
 const { runtime } = browser;
 
 /* listeners */
-runtime.onConnect.addListener(handleConnectedPort);
 runtime.onMessage.addListener(runtimeOnMsg);
 window.addEventListener('mousedown', handleBeforeContextMenu, true);
 window.addEventListener('keydown', handleKeyDown, true);
