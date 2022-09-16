cdfe3a6cdab66f5ce2daf53102f721f97460c0ad
asamuzaK (Kazz) <kazz@asamuzak.jp>
2022-09-16 19:35:12 +0900

    Add set icon badge function

--- src/mjs/icon.js
+++ src/mjs/icon.js
@@ -3,6 +3,7 @@
  */
 
 /* shared */
+import { isString } from './common.js';
 import { EXT_NAME, ICON, ICON_CONTEXT_ID, WEBEXT_ID } from './constant.js';
 
 /* api */
@@ -42,4 +43,29 @@ export const setDefaultIcon = async () => {
     func = setIcon(ICON_CONTEXT_ID);
   }
   return func || null;
+};
+
+/**
+ * set icon badge
+ *
+ * @param {object} opt - option
+ * @returns {Promise.<Array>} - result of each handler
+ */
+export const setIconBadge = async (opt = {}) => {
+  const { color, text } = opt;
+  const func = [];
+  if (color &&
+      (isString(color) ||
+       (Array.isArray(color) &&
+        color.every(i => Number.isInteger(i) && i >= 0 && i <= 255))) &&
+      isString(text)) {
+    func.push(
+      action.setBadgeBackgroundColor({ color }),
+      action.setBadgeText({ text })
+    );
+    if (text && typeof action.setBadgeTextColor === 'function') {
+      func.push(action.setBadgeTextColor({ color: 'white' }));
+    }
+  }
+  return Promise.all(func);
 };
--- test/icon.test.js
+++ test/icon.test.js
@@ -170,4 +170,106 @@ describe('icon', () => {
       ], 'result');
     });
   });
+
+  describe('set icon badge', () => {
+    const func = mjs.setIconBadge;
+
+    it('should not call function', async () => {
+      const i = browser.action.setBadgeBackgroundColor.callCount;
+      const j = browser.action.setBadgeText.callCount;
+      const k = browser.action.setBadgeTextColor.callCount;
+      const res = await func();
+      assert.strictEqual(browser.action.setBadgeBackgroundColor.callCount,
+        i, 'not called');
+      assert.strictEqual(browser.action.setBadgeText.callCount, j,
+        'not called');
+      assert.strictEqual(browser.action.setBadgeTextColor.callCount, k,
+        'not called');
+      assert.deepEqual(res, [], 'result');
+    });
+
+    it('should not call function', async () => {
+      const i = browser.action.setBadgeBackgroundColor.callCount;
+      const j = browser.action.setBadgeText.callCount;
+      const k = browser.action.setBadgeTextColor.callCount;
+      const res = await func({
+        color: [0, 0, 0, 0]
+      });
+      assert.strictEqual(browser.action.setBadgeBackgroundColor.callCount,
+        i, 'not called');
+      assert.strictEqual(browser.action.setBadgeText.callCount, j,
+        'not called');
+      assert.strictEqual(browser.action.setBadgeTextColor.callCount, k,
+        'not called');
+      assert.deepEqual(res, [], 'result');
+    });
+
+    it('should not call function', async () => {
+      const i = browser.action.setBadgeBackgroundColor.callCount;
+      const j = browser.action.setBadgeText.callCount;
+      const k = browser.action.setBadgeTextColor.callCount;
+      const res = await func({
+        color: [0, 0, 0, -1],
+        text: ''
+      });
+      assert.strictEqual(browser.action.setBadgeBackgroundColor.callCount,
+        i, 'not called');
+      assert.strictEqual(browser.action.setBadgeText.callCount, j,
+        'not called');
+      assert.strictEqual(browser.action.setBadgeTextColor.callCount, k,
+        'not called');
+      assert.deepEqual(res, [], 'result');
+    });
+
+    it('should not call function', async () => {
+      const i = browser.action.setBadgeBackgroundColor.callCount;
+      const j = browser.action.setBadgeText.callCount;
+      const k = browser.action.setBadgeTextColor.callCount;
+      const res = await func({
+        color: [0, 0, 0, 256],
+        text: ''
+      });
+      assert.strictEqual(browser.action.setBadgeBackgroundColor.callCount,
+        i, 'not called');
+      assert.strictEqual(browser.action.setBadgeText.callCount, j,
+        'not called');
+      assert.strictEqual(browser.action.setBadgeTextColor.callCount, k,
+        'not called');
+      assert.deepEqual(res, [], 'result');
+    });
+
+    it('should call function', async () => {
+      const i = browser.action.setBadgeBackgroundColor.callCount;
+      const j = browser.action.setBadgeText.callCount;
+      const k = browser.action.setBadgeTextColor.callCount;
+      const res = await func({
+        color: [0, 0, 0, 0],
+        text: ''
+      });
+      assert.strictEqual(browser.action.setBadgeBackgroundColor.callCount,
+        i + 1, 'called');
+      assert.strictEqual(browser.action.setBadgeText.callCount, j + 1,
+        'called');
+      assert.strictEqual(browser.action.setBadgeTextColor.callCount, k,
+        'not called');
+      assert.deepEqual(res, [undefined, undefined], 'result');
+    });
+
+    it('should call function', async () => {
+      const i = browser.action.setBadgeBackgroundColor.callCount;
+      const j = browser.action.setBadgeText.callCount;
+      const k = browser.action.setBadgeTextColor.callCount;
+      const res = await func({
+        color: 'red',
+        text: 'foo'
+      });
+      assert.strictEqual(browser.action.setBadgeBackgroundColor.callCount,
+        i + 1, 'called');
+      assert.strictEqual(browser.action.setBadgeText.callCount, j + 1,
+        'called');
+      assert.strictEqual(browser.action.setBadgeTextColor.callCount, k + 1,
+        'called');
+      assert.deepEqual(res, [undefined, undefined, undefined], 'result');
+    });
+  });
 });
