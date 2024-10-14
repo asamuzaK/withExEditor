e792645910d502a8f09bc698b11dada23e64f832
asamuzaK (Kazz) <kazz@asamuzak.jp>
2024-10-14 17:52:37 +0900

    Update types

--- types/src/mjs/dom-event.d.ts
+++ types/src/mjs/dom-event.d.ts
@@ -1,7 +1,7 @@
-export function dispatchEvent(target?: object, type?: string, opt?: object): boolean;
-export function dispatchChangeEvent(elm?: object): boolean;
-export function dispatchClipboardEvent(elm?: object, type?: string, opt?: object): boolean;
-export function dispatchFocusEvent(elm?: object): boolean;
-export function dispatchInputEvent(elm?: object, type?: string, opt?: object): boolean;
-export function dispatchKeyboardEvent(elm?: object, type?: string, keyOpt?: object): boolean;
+export function dispatchEvent(target: object, type: string, opt: object): boolean;
+export function dispatchChangeEvent(elm: object): boolean;
+export function dispatchClipboardEvent(elm: object, type: string, opt?: object): boolean;
+export function dispatchFocusEvent(elm: object): boolean;
+export function dispatchInputEvent(elm: object, type: string, opt: object): boolean;
+export function dispatchKeyboardEvent(elm: object, type: string, keyOpt?: object): boolean;
 export function focusElement(evt: object): object;
